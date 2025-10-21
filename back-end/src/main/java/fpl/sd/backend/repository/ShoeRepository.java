c6ed4b6ed6fcaf79c4c94ae099aae5259f436b13
Minh Quan <149597095+hmnhquaan@users.noreply.github.com>
2025-10-21 12:23:58 +0700

    Update ShoeRepository.java

--- back-end/src/main/java/fpl/sd/backend/repository/ShoeRepository.java
+++ back-end/src/main/java/fpl/sd/backend/repository/ShoeRepository.java
@@ -38,7 +38,6 @@ public interface ShoeRepository extends JpaRepository<Shoe, Integer> {
     AND (:brandId IS NULL OR b.id = :brandId)
     AND (:gender IS NULL OR s.gender = :gender)
     AND (:category IS NULL OR s.category = :category)
-    AND (:status IS NULL OR s.status = :status)
     """)
     Page<Shoe> findShoesByFilters(@Param("name") String name,
                                 @Param("minPrice") Long minPrice,
